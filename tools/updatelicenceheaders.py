7a372ac6af8af697a96597769ac5002b85840c93
uuk0 <baulukas1301@googlemail.com>
2019-07-13 07:49:39 +0200

    added licence header updater

---
+++ tools/updatelicenceheaders.py
@@ -0,0 +1,28 @@
+"""mcpython - a minecraft clone written in python licenced under MIT-licence
+authors: uuk"""
+import os
+
+
+def update_licence_headers_in_project(project_home, header):
+    for root, _, files in os.walk(project_home, topdown=False):
+        for file in files:
+            if file.endswith(".py"):
+                cfile = os.path.join(root, file)
+                with open(cfile) as f:
+                    data = f.read()
+                if not data.startswith(header):
+                    if data.startswith("'''"):
+                        data = header + data[data.index("'''", 3)+3:]
+                    elif data.startswith('"""'):
+                        data = header + data[data.index('"""', 3)+3:]
+                    else:
+                        data = header + "\n" + data
+                with open(cfile, mode="w") as f:
+                    f.write(data)
+
+
+if __name__ == "__main__":
+    update_licence_headers_in_project(os.path.dirname(os.path.dirname(__file__)),
+                                      '''"""mcpython - a minecraft clone written in python licenced under MIT-licence
+authors: uuk"""''')
+
