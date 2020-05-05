53d971afc9311b59b5016aa723b3f12c8c14d8e6
uuk0 <baulukas1301@googlemail.com>
2020-05-05 18:26:33 +0200

    improved personal directory logging

--- logger.py
+++ logger.py
@@ -44,6 +44,7 @@ inter_home = os.path.dirname(sys.executable).replace("\\", "/")
 
 
 def println(*msg, sep=" ", end="\n", write_into_console=True):
+    msg = [str(e).replace("\\", "/").replace(G.local, "%LOCAL%").replace(inter_home, "%PYTHON%") for e in msg]
     if write_into_console: print(*msg, sep=sep, end=end)
     with open(log_file, mode="a") as f:
         # print(datetime.now().strftime("\n[timestamp: %H.%M.%S]"), file=f)
