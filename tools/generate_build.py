203aafb3337f1cc7701b316cc2d516d84f6dc770
uuk0 <baulukas1301@googlemail.com>
2020-07-21 19:52:46 +0200

    build system will generate now also undocumented code

--- tools/generate_build.py
+++ tools/generate_build.py
@@ -26,7 +26,18 @@ how to build an final version?
 6. collect the assets
 7. exchange build information in globals.py from dev-environment to build
 8. zip-up the files into one directory
+9. remove documentation strings
+10. zip-up the undocumented code
 """
+
+
+def collect_to_zip(source, file):
+    with zipfile.ZipFile(file, mode="w") as f:
+        for root, dirs, files in os.walk(source):
+            for file in files:
+                t = os.path.join(root, file)
+                m = t[len(source) + 1:]
+                f.write(t, m)
 
 
 def _copytree(entries, src, dst, symlinks, ignore, copy_function,
@@ -141,8 +152,8 @@ def build():
     print("collecting changelog...")
     shutil.copy(local + "/changelog.txt", folder + "/changelog.txt")
 
-    print("collecting data generations...")
-    # 1. we don't want an window, 2. we want to include ALL possible block-data (is selective loaded with the EU config)
+    print("collecting data generators...")
+    # 1. we don't want an window, 2. we want to include ALL possible block-data (is selective loaded with the End User config)
     subprocess.call(["py", "-3.8", local+"/__main__.py", "--data-gen", "--exit-after-data-gen", "--no-window",
                      "--enable-all-blocks"], stdout=sys.stdout, stderr=sys.stderr)  # and write console to these console
 
@@ -173,14 +184,81 @@ def build():
 
     print("zip-ing up stuff...")
     now = datetime.datetime.now()
-    with zipfile.ZipFile(out + "/build_{}_{}_{}_{}_{}_{}.zip".format(
-            now.year, now.month, now.day, now.hour, now.minute, now.second), mode="w") as f:
-        for root, dirs, files in os.walk(folder):
-            for file in files:
-                t = os.path.join(root, file)
-                m = t[len(folder) + 1:]
-                f.write(t, m)
+    target_file = "build_{}_{}_{}_{}_{}_{}".format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+    collect_to_zip(folder, out + "/" + target_file + ".zip")
+
+    print("removing documentation...")
+
+    root_l = len(folder) + 1
+    for root, dirs, files in os.walk(folder):
+        for loc in files:
+            if not loc.endswith(".py"): continue  # only python files to work with
+            file = os.path.join(root, loc)
+            print("transforming file '{}'".format(file[root_l:]))
+            with open(file) as f:
+                data = f.readlines()
+
+            result = []  # here we store the context
+            in_multi_line_comment = 0
+            for line_n, line in enumerate(data):
+                line = line[:-1]
+                multi_line_change = False
+                index = None
+                in_string = 0
+                skip_entries = 0
+                for i, e in enumerate(line):
+                    if e == '"' and not (i > 0 and line[i - 1] == "\\"):
+                        if len(line) > i + 1 and line[i:i + 3] == '"""':
+                            if in_multi_line_comment == 0:
+                                in_multi_line_comment = 1
+                                line = line[:index]
+                                multi_line_change = True
+                            elif in_multi_line_comment == 1:
+                                in_multi_line_comment = 0
+                                multi_line_change = True
+                        elif in_string == 0:
+                            in_string = 1
+                        elif in_string == 1:
+                            in_string = 0
+                    elif e == "'" and not (i > 0 and line[i - 1] == "\\"):
+                        if len(line) > i + 1 and line[i:i + 3] == "'''":
+                            if in_multi_line_comment == 0:
+                                in_multi_line_comment = 2
+                                line = line[:index]
+                                multi_line_change = True
+                            elif in_multi_line_comment == 2:
+                                in_multi_line_comment = 0
+                                multi_line_change = True
+                        elif in_string == 0:
+                            in_string = 2
+                        elif in_string == 2:
+                            in_string = 0
+                    elif e == "#" and in_string == 0 and index is None:
+                        index = i
+                        break
+                if index is not None:
+                    line = line[:index]
+                if not (not multi_line_change and in_multi_line_comment != 0):
+                    result.append(line)
+
+            with open(file, mode="w") as f:
+                i = 0
+                while i < len(result):
+                    line = result[i]
+                    if len(line.strip()) == 0 or line.strip() in ("'''", '"""'):
+                        result.pop(i)
+                    else:
+                        i += 1
+                f.write("\n".join(result))
+
+    print("zip-ing up...")
+    collect_to_zip(folder, out + "/" + target_file + "_undocumented.zip")
+
+    shutil.rmtree(folder)
+
     print("build finished in {}s".format(time.time() - start))
+    return target_file+".zip", target_file+"_undocumented.zip"
 
 
 if __name__ == "__main__":
