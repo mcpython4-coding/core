95854f6ac73a24b295269d64effd8a104f02d6af
uuk0 <baulukas1301@googlemail.com>
2020-12-10 10:49:52 +0100

    improved registry miss-match error message

--- mcpython/server/storage/serializer/RegistryInfo.py
+++ mcpython/server/storage/serializer/RegistryInfo.py
@@ -27,14 +27,16 @@ class RegistryInfo(mcpython.server.storage.serializer.IDataSerializer.IDataSeria
         if data is None:
             return
 
+        registry_miss_match = {}
+        registry_type_changes = []
+
         for registry in G.registry.registries:  # iterate over all registries
             if not registry.dump_content_in_saves:
                 continue
+            registry_miss_match[registry.name] = []
             if registry.name not in data:
-                logger.println(
-                    "[REGISTRY][WARN] registry '{}' not found in files!".format(
-                        registry.name
-                    )
+                registry_type_changes.append(
+                    "registry {} not dumped in save files".format(registry.name)
                 )
             else:
                 entries = data[registry.name]
@@ -42,24 +44,28 @@ class RegistryInfo(mcpython.server.storage.serializer.IDataSerializer.IDataSeria
                 for obj in registry.entries.values():
                     compressed = obj.compressed_info()
                     if compressed not in entries:
-                        logger.println(
-                            "[REGISTRY][WARN] object '{}' in registry '{}' not found in saves!".format(
-                                obj.NAME, registry.name
-                            )
+                        registry_miss_match[registry.name].append(
+                            "new object: {}".format(obj.NAME)
                         )
                     else:
                         entries.remove(compressed)
                 for compressed in entries:
-                    logger.println(
-                        "[REGISTRY][WARN] compressed info '{}' for registry '{}' found in saves, but not in"
-                        " active registry".format(compressed, registry.name)
+                    registry_miss_match[registry.name].append(
+                        "missing object: {}".format(compressed)
                     )
         for name in data:
-            logger.println(
-                "[REGISTRY][WARN] registry '{}' found in saves, but it is not arrival anymore".format(
-                    name
-                )
+            registry_type_changes.append("missing registry: {}".format(name))
+
+        if len(registry_type_changes) != 0:
+            logger.write_into_container(
+                registry_type_changes, header="REGISTRY TYPE CHANGES"
             )
+        for name in registry_miss_match:
+            if len(registry_miss_match[name]) != 0:
+                logger.write_into_container(
+                    registry_miss_match[name],
+                    header="registry {} has the following miss-matches".format(name),
+                )
 
     @classmethod
     def save(cls, data, savefile):
