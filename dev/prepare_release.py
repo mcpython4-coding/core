929d24ebcb43d67ef13fd173eca2bd7ad9467c7f
uuk0 <baulukas1301@googlemail.com>
2021-02-09 09:05:32 +0100

    new licence header

--- dev/prepare_release.py
+++ dev/prepare_release.py
@@ -1,10 +1,13 @@
 """
-mcpython - a minecraft clone written in python licenced under MIT-licence
-authors: uuk, xkcdjerry (inactive)
+mcpython - a minecraft clone written in python licenced under the MIT-licence 
+(https://github.com/mcpython4-coding/core)
 
-based on the game of fogleman (https://github.com/fogleman/Minecraft) licenced under MIT-licence
-original game "minecraft" by Mojang Studios (www.minecraft.net)
-mod loader inspired by "minecraft forge" (https://github.com/MinecraftForge/MinecraftForge)
+Contributors: uuk, xkcdjerry (inactive)
+
+Based on the game of fogleman (https://github.com/fogleman/Minecraft), licenced under the MIT-licence
+Original game "minecraft" by Mojang Studios (www.minecraft.net), licenced under the EULA
+(https://account.mojang.com/documents/minecraft_eula)
+Mod loader inspired by "Minecraft Forge" (https://github.com/MinecraftForge/MinecraftForge) and similar
 
 This project is not official by mojang and does not relate to it.
 """
@@ -30,7 +33,7 @@ with open(home + "/version.json", mode="w") as f:
 
 data["name"] = input("release name: ")
 
-generate_build.main(data["name"]+" final")
+generate_build.main(data["name"] + " final")
 
 data["name"] = "test build for " + input("version after this release: ") + " 0"
 data["preview_build_counter"] = 1
