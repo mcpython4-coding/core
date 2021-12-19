7046fe84d3cec6667cc80cac33ff55139ee54c56
uuk0 <baulukas1301@googlemail.com>
2021-12-19 10:54:41 +0100

    [f] So, here is the smithing table recipe implementation
    Signed-off-by: uuk0 <baulukas1301@googlemail.com>

--- mcpython/common/container/crafting/SmithingRecipe.py
+++ mcpython/common/container/crafting/SmithingRecipe.py
@@ -11,22 +11,30 @@ Mod loader inspired by "Minecraft Forge" (https://github.com/MinecraftForge/Mine
 
 This project is not official by mojang and does not relate to it.
 """
+import typing
+
 import mcpython.common.container.crafting.GridRecipeInstances
 import mcpython.common.container.crafting.IRecipe
 from mcpython import shared
+from mcpython.common.container.ResourceStack import ItemStack
+from .GridRecipeInstances import transform_to_item_stack
 
 
 @shared.crafting_handler
 class SmithingRecipe(mcpython.common.container.crafting.IRecipe.IRecipe):
-    # todo: implement
+    __slots__ = mcpython.common.container.crafting.IRecipe.IRecipe.__slots__ + ("base", "addition", "output")
+
     # The list of type descriptors to decode
     RECIPE_TYPE_NAMES = ["minecraft:smithing"]
     CRAFTING_SUPPORT = ["minecraft:smithing_table"]
 
     @classmethod
     def from_data(cls, data: dict, file: str) -> "SmithingRecipe":
-        pass
-        return cls()
+        output = transform_to_item_stack(data["result"], file)
+        return cls(list(map(lambda e: e[0], transform_to_item_stack(data["base"], file))), list(map(lambda e: e[0], transform_to_item_stack(data["addition"], file))), output[0][0] if output else None)
 
-    def __init__(self):
+    def __init__(self, base: typing.List[str], addition: typing.List[str], output: str):
         super().__init__()
+        self.base = base
+        self.addition = addition
+        self.output = output
