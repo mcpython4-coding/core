9de8f845c31473bb9464e41241106a1800317a42
uuk0 <baulukas1301@googlemail.com>
2022-01-02 15:02:55 +0100

    [O] small optimization to player load/save
    Signed-off-by: uuk0 <baulukas1301@googlemail.com>

--- mcpython/common/world/serializer/PlayerData.py
+++ mcpython/common/world/serializer/PlayerData.py
@@ -13,6 +13,8 @@ This project is not official by mojang and does not relate to it.
 """
 import asyncio
 
+import typing
+
 import mcpython.common.world.datafixers.IDataFixer
 from mcpython import shared
 from mcpython.engine.network.util import ReadBuffer
@@ -45,13 +47,15 @@ class PlayerData(mcpython.common.world.serializer.IDataSerializer.IDataSerialize
 
         current_player = shared.world.get_active_player().name if shared.IS_CLIENT else None
 
-        for player_data in players:
+        async def load_player(player_data: bytes):
             player_buffer = ReadBuffer(player_data)
             player_name = player_buffer.read_string()
 
             if not shared.IS_CLIENT or player_name == current_player:
                 player = await shared.world.get_player_by_name_async(player_name)
                 await player.read_from_network_buffer(player_buffer)
+
+        await asyncio.gather(*(load_player(player_data) for player_data in players))
 
     @classmethod
     async def save(cls, data, save_file, **_):
@@ -69,16 +73,24 @@ class PlayerData(mcpython.common.world.serializer.IDataSerializer.IDataSerialize
 
         player_names = [ReadBuffer(e).read_string() for e in players]
 
-        for player in shared.world.players.values():
+        async def assemble_player_data(player) -> typing.Tuple[int, bytes]:
             p_buffer = WriteBuffer()
             p_buffer.write_string(player.name)
             await player.write_to_network_buffer(p_buffer)
             data = p_buffer.get_data()
 
             if player.name in player_names:
-                players[player_names.index(player.name)] = data
+                return player_names.index(player.name), data
             else:
+                return -1, data
+
+        additional = await asyncio.gather(*(assemble_player_data(p) for p in shared.world.players.values()))
+
+        for index, data in additional:
+            if index == -1:
                 players.append(data)
+            else:
+                players[index] = data
 
         write_buffer = WriteBuffer()
         write_buffer.write_uint(cls.FILE_VERSION)
