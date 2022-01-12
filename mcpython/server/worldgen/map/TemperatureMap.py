773a9bddd601987213cb2a89741ddde60fa0d1f0
uuk0 <baulukas1301@googlemail.com>
2022-01-12 14:41:43 +0100

    [I] optimised the temperature map data structure
    Signed-off-by: uuk0 <baulukas1301@googlemail.com>

--- mcpython/server/worldgen/map/TemperatureMap.py
+++ mcpython/server/worldgen/map/TemperatureMap.py
@@ -11,8 +11,8 @@ Mod loader inspired by "Minecraft Forge" (https://github.com/MinecraftForge/Mine
 
 This project is not official by mojang and does not relate to it.
 """
+import array
 import itertools
-import typing
 
 import mcpython.server.worldgen.map.AbstractChunkInfoMap
 import PIL.Image
@@ -25,38 +25,31 @@ class TemperatureMap(mcpython.server.worldgen.map.AbstractChunkInfoMap.AbstractM
 
     def __init__(self, chunk):
         super().__init__(chunk)
-        self.temperature_map: typing.Dict[typing.Tuple[int, int], float] = {}
+        self.temperature_data = array.ArrayType("f", (0,) * 256)
 
     async def write_to_network_buffer(self, buffer: WriteBuffer):
         await super().write_to_network_buffer(buffer)
-
-        cx, cz = self.chunk.get_position()
-        cx *= 16
-        cz *= 16
-
-        for x, z in itertools.product(range(16), range(16)):
-            buffer.write_float(self.get_at_xz(cx + x, cz + z))
+        for e in self.temperature_data:
+            buffer.write_float(e)
 
     async def read_from_network_buffer(self, buffer: ReadBuffer):
         await super().read_from_network_buffer(buffer)
 
-        cx, cz = self.chunk.get_position()
-        cx *= 16
-        cz *= 16
-
-        for x, z in itertools.product(range(16), range(16)):
-            self.set_at_xz(x + cx, z + cz, buffer.read_float())
+        self.temperature_data[:] = array.ArrayType("f", [buffer.read_float() for _ in range(256)])
 
     def get_at_xz(self, x: int, z: int) -> float:
-        return self.temperature_map[x, z] if (x, z) in self.temperature_map else 0
+        return self.temperature_data[x % 16 + z % 16 * 16]
 
     def set_at_xz(self, x: int, z: int, temperature: float):
-        self.temperature_map[x, z] = temperature
+        self.temperature_data[x % 16 + z % 16 * 16] = temperature
 
     def dump_debug_info(self, file: str):
         image = PIL.Image.new("RGBA", (16, 16))
-        for (x, z), temp in self.temperature_map.items():
+
+        for x, z in itertools.product(range(16), repeat=2):
+            temp = self.get_at_xz(x, z)
             image.putpixel((x % 16, z % 16), temp * 255 % 256)
+
         image.save(file)
 
 
