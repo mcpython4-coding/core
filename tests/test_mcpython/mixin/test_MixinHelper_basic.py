3e9962bd4ad84e4132ead1e2c38d6c07382a4766
uuk0 <baulukas1301@googlemail.com>
2022-01-14 21:15:48 +0100

    [IT] moved some unit tests to another file
    Signed-off-by: uuk0 <baulukas1301@googlemail.com>

---
+++ tests/test_mcpython/mixin/test_MixinHelper_basic.py
@@ -0,0 +1,127 @@
+import test_mcpython.mixin
+from util import TestCase
+
+
+class TestBasicBytecodeHelpers(TestCase):
+    def test_mixin_static_method_call(self):
+        from mcpython.mixin.MixinMethodWrapper import FunctionPatcher, MixinPatchHelper
+
+        def localtest():
+            return 0
+
+        patcher = FunctionPatcher(localtest)
+        helper = MixinPatchHelper(patcher)
+        helper.insertStaticMethodCallAt(
+            0, "test_mcpython.mixin.test_space:test_for_invoke"
+        )
+        helper.store()
+        patcher.applyPatches()
+
+        count = test_mcpython.mixin.test_space.INVOKED
+        self.assertEqual(localtest(), 0)
+        self.assertEqual(test_mcpython.mixin.test_space.INVOKED, count + 1)
+        test_mcpython.mixin.test_space.INVOKED = 0
+
+    def test_mixin_static_method_call_twice(self):
+        from mcpython.mixin.MixinMethodWrapper import FunctionPatcher, MixinPatchHelper
+
+        def localtest():
+            return 0
+
+        patcher = FunctionPatcher(localtest)
+        helper = MixinPatchHelper(patcher)
+        helper.insertStaticMethodCallAt(
+            0, "test_mcpython.mixin.test_space:test_for_invoke"
+        )
+        helper.insertStaticMethodCallAt(
+            0, "test_mcpython.mixin.test_space:test_for_invoke"
+        )
+        helper.store()
+        patcher.applyPatches()
+
+        count = test_mcpython.mixin.test_space.INVOKED
+        self.assertEqual(localtest(), 0)
+        self.assertEqual(test_mcpython.mixin.test_space.INVOKED, count + 2)
+        test_mcpython.mixin.test_space.INVOKED = 0
+
+    async def test_mixin_static_method_call_to_async(self):
+        from mcpython.mixin.MixinMethodWrapper import FunctionPatcher, MixinPatchHelper
+
+        async def localtest():
+            return 0
+
+        patcher = FunctionPatcher(localtest)
+        helper = MixinPatchHelper(patcher)
+        helper.insertAsyncStaticMethodCallAt(
+            0, "test_mcpython.mixin.test_space:test_for_invoke_async"
+        )
+        helper.store()
+        patcher.applyPatches()
+
+        count = test_mcpython.mixin.test_space.INVOKED
+        self.assertEqual(await localtest(), 0)
+        self.assertEqual(test_mcpython.mixin.test_space.INVOKED, count + 1)
+        test_mcpython.mixin.test_space.INVOKED = 0
+
+    async def test_mixin_static_method_call_to_async_twice(self):
+        from mcpython.mixin.MixinMethodWrapper import FunctionPatcher, MixinPatchHelper
+
+        async def localtest():
+            return 0
+
+        patcher = FunctionPatcher(localtest)
+        helper = MixinPatchHelper(patcher)
+        helper.insertAsyncStaticMethodCallAt(
+            0, "test_mcpython.mixin.test_space:test_for_invoke_async"
+        )
+        helper.insertAsyncStaticMethodCallAt(
+            0, "test_mcpython.mixin.test_space:test_for_invoke_async"
+        )
+        helper.store()
+        patcher.applyPatches()
+
+        count = test_mcpython.mixin.test_space.INVOKED
+        self.assertEqual(await localtest(), 0)
+        self.assertEqual(test_mcpython.mixin.test_space.INVOKED, count + 2)
+        test_mcpython.mixin.test_space.INVOKED = 0
+
+    async def test_mixin_static_method_call_async_context(self):
+        from mcpython.mixin.MixinMethodWrapper import FunctionPatcher, MixinPatchHelper
+
+        async def localtest():
+            return 0
+
+        patcher = FunctionPatcher(localtest)
+        helper = MixinPatchHelper(patcher)
+        helper.insertStaticMethodCallAt(
+            1, "test_mcpython.mixin.test_space:test_for_invoke"
+        )
+        helper.store()
+        patcher.applyPatches()
+
+        count = test_mcpython.mixin.test_space.INVOKED
+        self.assertEqual(await localtest(), 0)
+        self.assertEqual(test_mcpython.mixin.test_space.INVOKED, count + 1)
+        test_mcpython.mixin.test_space.INVOKED = 0
+
+    async def test_mixin_static_method_call_async_context_twice(self):
+        from mcpython.mixin.MixinMethodWrapper import FunctionPatcher, MixinPatchHelper
+
+        async def localtest():
+            return 0
+
+        patcher = FunctionPatcher(localtest)
+        helper = MixinPatchHelper(patcher)
+        helper.insertStaticMethodCallAt(
+            1, "test_mcpython.mixin.test_space:test_for_invoke"
+        )
+        helper.insertStaticMethodCallAt(
+            1, "test_mcpython.mixin.test_space:test_for_invoke"
+        )
+        helper.store()
+        patcher.applyPatches()
+
+        count = test_mcpython.mixin.test_space.INVOKED
+        self.assertEqual(await localtest(), 0)
+        self.assertEqual(test_mcpython.mixin.test_space.INVOKED, count + 2)
+        test_mcpython.mixin.test_space.INVOKED = 0
